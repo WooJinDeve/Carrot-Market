e4e9450397f137d381f3dc9be00a70df9357f03c
WoojinDeve <jwj060111@gmail.com>
2022-12-28 21:50:49 +0900

    init: 이미지 서버 프로젝트 생성

---
+++ imageserver/src/test/java/com/carrot/imageserver/ImageserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrot.imageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
