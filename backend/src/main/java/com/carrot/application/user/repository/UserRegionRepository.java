27beec25a6359af906f98657647996c99f9987d4
WoojinDeve <jwj060111@gmail.com>
2022-12-30 16:13:30 +0900

    feat: 지역 서비스 클래스 테스트 코드

--- backend/src/main/java/com/carrot/application/user/repository/UserRegionRepository.java
+++ backend/src/main/java/com/carrot/application/user/repository/UserRegionRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 import static com.carrot.global.error.ErrorCode.USER_REGION_NOTFOUND_ERROR;
 
@@ -15,7 +14,7 @@ public interface UserRegionRepository extends JpaRepository<UserRegion, Long> {
 
     @Query("SELECT u FROM UserRegion u JOIN FETCH u.region WHERE u.user.id = :id")
     List<UserRegion> findByIdAndFetchRegion(@Param("id") Long userId);
-    Optional<UserRegion> findByUserIdAndRegionId(Long userId, Long regionId);
+
     boolean existsByUserIdAndRegionId(Long userId, Long regionId);
 
     long countByUserId(Long userId);
--- backend/src/test/java/com/carrot/testutil/RepositoryTest.java
+++ backend/src/test/java/com/carrot/testutil/RepositoryTest.java
@@ -1,7 +1,10 @@
 package com.carrot.testutil;
 
+import com.carrot.global.config.JpaConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @DataJpaTest
+@Import(JpaConfiguration.class)
 public class RepositoryTest {
 }
