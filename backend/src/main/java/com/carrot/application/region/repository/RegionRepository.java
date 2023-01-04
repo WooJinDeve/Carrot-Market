0ea4508a58fa32e46731223acd58a3f22081910a
WoojinDeve <jwj060111@gmail.com>
2023-01-04 15:38:58 +0900

    faet & fix: 사용자 주변지역 조회 및 BatchInsert 삭제

--- backend/src/main/java/com/carrot/application/region/repository/RegionRepository.java
+++ backend/src/main/java/com/carrot/application/region/repository/RegionRepository.java
@@ -2,15 +2,22 @@ package com.carrot.application.region.repository;
 
 import com.carrot.application.region.domain.Region;
 import com.carrot.global.error.CarrotRuntimeException;
+import org.locationtech.jts.geom.Geometry;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 import static com.carrot.global.error.ErrorCode.REGION_NOTFOUND_ERROR;
 
 public interface RegionRepository extends JpaRepository<Region, Long> {
 
     Slice<Region> findByNameContaining(String name, Pageable pageable);
+
+    @Query("SELECT r.id FROM Region r WHERE within(r.location, :filter) = true")
+    List<Long> findRegionWithin(Geometry filter);
 
     default Region getById(Long id){
         return findById(id)
--- backend/src/main/java/com/carrot/application/region/service/RegionService.java
+++ backend/src/main/java/com/carrot/application/region/service/RegionService.java
@@ -1,12 +1,14 @@
 package com.carrot.application.region.service;
 
 import com.carrot.application.region.domain.Region;
-import com.carrot.application.region.repository.RegionJdbcRepository;
 import com.carrot.application.region.repository.RegionRepository;
 import com.carrot.infrastructure.util.CsvUtil;
 import com.carrot.infrastructure.util.PointUtil;
 import com.carrot.presentation.response.RegionSearchResponse;
 import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.util.GeometricShapeFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
@@ -24,7 +26,6 @@ public class RegionService {
 
     private static final String CSV_LOCATION = "/csv/location.csv";
     private final RegionRepository regionRepository;
-    private final RegionJdbcRepository regionJdbcRepository;
 
     public Slice<RegionSearchResponse> search(String state, Pageable pageable) {
         return regionRepository.findByNameContaining(state, pageable)
@@ -33,21 +34,34 @@ public class RegionService {
 
     @PostConstruct
     @Transactional
-    public void insert(){
-        if(regionRepository.count() == 0){
+    public void insert() {
+        if (regionRepository.count() == 0) {
             List<Region> regions = convertToRegions();
-            regionJdbcRepository.batchInsert(regions);
+            regionRepository.saveAll(regions);
         }
     }
 
-    private List<Region> convertToRegions(){
+    public List<Long> findByGeometryRegions(final Double latitude, final Double longitude, final Integer distance) {
+        final Geometry circle = createCircle(latitude, longitude, distance);
+        return regionRepository.findRegionWithin(circle);
+    }
+
+    private Geometry createCircle(Double latitude, Double longitude, Integer radius) {
+        GeometricShapeFactory geometricShapeFactory = new GeometricShapeFactory();
+        geometricShapeFactory.setNumPoints(32);
+        geometricShapeFactory.setCentre(new Coordinate(latitude, longitude));
+        geometricShapeFactory.setSize(radius * 2);
+        return geometricShapeFactory.createCircle();
+    }
+
+    private List<Region> convertToRegions() {
         return Objects.requireNonNull(CsvUtil.readCsvLines(CSV_LOCATION))
                 .stream()
                 .map(this::convertToRegion)
                 .collect(Collectors.toList());
     }
 
-    private Region convertToRegion(String[] regions){
+    private Region convertToRegion(String[] regions) {
         return Region.of(regions[0], regions[1], PointUtil.createPoint(regions[2], regions[3]));
     }
 }
