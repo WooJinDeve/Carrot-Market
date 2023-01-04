b2c300277fad73393f44d9330b04d73b4ef78a83
WoojinDeve <jwj060111@gmail.com>
2023-01-04 13:11:30 +0900

    refactor: namedJdbcTemplate -> jdbcTemplate 변경 및 batchInsert 메서드 구현

--- backend/src/main/java/com/carrot/application/region/repository/RegionJdbcRepository.java
+++ backend/src/main/java/com/carrot/application/region/repository/RegionJdbcRepository.java
@@ -1,32 +1,36 @@
 package com.carrot.application.region.repository;
 
 import com.carrot.application.region.domain.Region;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
+
+import static java.time.LocalDateTime.now;
+
 
 @Repository
 public class RegionJdbcRepository {
-
     private static final String TABLE = "region";
 
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-    public RegionJdbcRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    public RegionJdbcRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void bulkInsert(List<Region> regions) {
+    public void batchInsert(List<Region> regions) {
         String sql = String.format(
                 "INSERT INTO `%s` (region_code, region_name, location, created_at, updated_at) " +
-                "VALUES (:regionCode, :name, ST_GeomFromText(':location'), :createdAt, :updatedAt)", TABLE);
+                        "VALUES (?, ?, ?, ?, ?)", TABLE);
 
-        SqlParameterSource[] params = regions.stream()
-                .map(BeanPropertySqlParameterSource::new)
-                .toArray(SqlParameterSource[]::new);
-        namedParameterJdbcTemplate.batchUpdate(sql, params);
+        jdbcTemplate.batchUpdate(sql, regions, 500, (ps, argument) -> {
+            ps.setString(1, argument.getRegionCode());
+            ps.setString(2, argument.getName());
+            ps.setObject(3, argument.getLocation());
+            ps.setTimestamp(4, Timestamp.valueOf(now()));
+            ps.setTimestamp(5, Timestamp.valueOf(now()));
+        });
     }
 }
