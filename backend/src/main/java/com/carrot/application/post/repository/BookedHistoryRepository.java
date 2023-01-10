aa794485f6d3cbc3077b5bce1ee8b33c4ec74310
WoojinDeve <jwj060111@gmail.com>
2023-01-10 14:54:55 +0900

    fix & feat: 깃액션 테스트 도커 환경 추가 및 조회 기능 추가

--- backend/src/main/java/com/carrot/application/post/repository/BookedHistoryRepository.java
+++ backend/src/main/java/com/carrot/application/post/repository/BookedHistoryRepository.java
@@ -4,7 +4,6 @@ import com.carrot.application.post.domain.entity.BookedHistory;
 import com.carrot.global.error.CarrotRuntimeException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -13,7 +12,7 @@ import static com.carrot.global.error.ErrorCode.BOOKED_NOTFOUND_ERROR;
 public interface BookedHistoryRepository extends JpaRepository<BookedHistory, Long> {
 
     @Query("select bh from BookedHistory bh join fetch bh.post where bh.post.id = :postId")
-    Optional<BookedHistory> findByPostIdWithPost(@Param("postId") Long postId);
+    Optional<BookedHistory> findByPostIdWithPost(Long postId);
 
     default BookedHistory getByPostIdWithPost(Long postId){
         return findByPostIdWithPost(postId)
--- backend/src/main/java/com/carrot/application/post/repository/TransactionHistoryRepository.java
+++ backend/src/main/java/com/carrot/application/post/repository/TransactionHistoryRepository.java
@@ -1,7 +1,16 @@
 package com.carrot.application.post.repository;
 
 import com.carrot.application.post.domain.entity.TransactionHistory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long> {
+
+    @Query(value = "SELECT th FROM TransactionHistory th JOIN FETCH th.post WHERE th.buyer.id = :buyerId")
+    Slice<TransactionHistory> findAllByBuyerIdOrderByIdDesc(Long buyerId, Pageable pageable);
+
+    @Query(value = "SELECT th FROM TransactionHistory th JOIN FETCH th.post WHERE th.seller.id = :sellerId")
+    Slice<TransactionHistory> findAllBySellerIdOrderByIdDesc(Long sellerId, Pageable pageable);
 }
