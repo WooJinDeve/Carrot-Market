61488ac8bc1a0a8a8557f260d26aee3d500d3d6c
WoojinDeve <jwj060111@gmail.com>
2023-01-12 10:19:55 +0900

    feat: 소켓 컨트롤러 구현

--- backend/src/main/java/com/carrot/presentation/controller/ChatSocketController.java
+++ backend/src/main/java/com/carrot/presentation/controller/ChatSocketController.java
@@ -1,11 +1,34 @@
 package com.carrot.presentation.controller;
 
+import com.carrot.application.chat.service.ChatWriteService;
+import com.carrot.global.common.Response;
+import com.carrot.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+
+import static com.carrot.presentation.request.ChatRequest.ChatSendMessage;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ChatSocketController {
+
+    private final ChatWriteService chatService;
+    @MessageMapping("/chatRooms/{id}")
+    public void send(@DestinationVariable Long id, ChatSendMessage chatSendMessage) {
+        chatService.send(id, chatSendMessage.getUserId(), chatSendMessage.getContent());
+    }
+
+    @MessageExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleException(RuntimeException e) {
+        log.error("[Error] Internal Server Error ", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Response.error(ErrorCode.INTERNAL_SERVER_ERROR.name()));
+    }
 }
