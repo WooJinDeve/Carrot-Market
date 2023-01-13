98ea4d0217c65e1ac8c73320e94cf5e70363c89c
WoojinDeve <jwj060111@gmail.com>
2023-01-13 15:28:49 +0900

    feat: 알림 UI 로직 설계

---
+++ backend/src/main/java/com/carrot/presentation/controller/NotificationController.java
@@ -0,0 +1,33 @@
+package com.carrot.presentation.controller;
+
+import com.carrot.application.notification.service.NotificationReadService;
+import com.carrot.application.notification.service.NotificationWriteService;
+import com.carrot.application.user.dto.LoginUser;
+import com.carrot.global.common.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+
+@RestController
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationReadService notificationReadService;
+    private final NotificationWriteService notificationWriteService;
+
+    @GetMapping("/alarm")
+    public Response<?> alarm(@AuthenticationPrincipal LoginUser loginUser,
+                             Pageable pageable){
+
+        return null;
+    }
+
+    @GetMapping("/notification/subscribe")
+    public SseEmitter subscribe(@AuthenticationPrincipal LoginUser loginUser){
+        return null;
+    }
+}
