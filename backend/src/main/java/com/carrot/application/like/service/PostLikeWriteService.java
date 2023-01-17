0f0d657b1d3bd154014d2c21625a83abab953385
WoojinDeve <jwj060111@gmail.com>
2023-01-17 11:20:33 +0900

    feat: 좋아요 알림 이벤트 추가

--- backend/src/main/java/com/carrot/application/like/service/PostLikeWriteService.java
+++ backend/src/main/java/com/carrot/application/like/service/PostLikeWriteService.java
@@ -1,6 +1,7 @@
 package com.carrot.application.like.service;
 
 import com.carrot.application.like.domain.PostLike;
+import com.carrot.application.like.event.PostLikeNotificationEvent;
 import com.carrot.application.like.repository.PostLikeRepository;
 import com.carrot.application.post.domain.entity.Post;
 import com.carrot.application.post.repository.PostRepository;
@@ -9,6 +10,7 @@ import com.carrot.application.user.repository.UserRepository;
 import com.carrot.application.user.service.UserValidator;
 import com.carrot.global.error.CarrotRuntimeException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +25,9 @@ public class PostLikeWriteService {
     private final UserRepository userRepository;
     private final UserValidator userValidator;
 
+    private final ApplicationEventPublisher publisher;
+
+
     public void like(Long userId, Long postId){
         User user = userRepository.getById(userId);
         userValidator.validateDeleted(user);
@@ -33,7 +38,7 @@ public class PostLikeWriteService {
         postLikeRepository.verifyExistByUserIdAndPostId(userId, postId);
         postLikeRepository.save(PostLike.of(post, user));
 
-        //TODO: Alarm
+        publisher.publishEvent(new PostLikeNotificationEvent(userId, postId, post.getUser().getId()));
     }
 
     public void cancel(Long userId, Long postId){
