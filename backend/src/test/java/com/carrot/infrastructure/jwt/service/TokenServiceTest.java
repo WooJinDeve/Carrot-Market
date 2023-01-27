a2f7ab0fc1b53f03eb2fe5ac5793d3fbeb81e90f
WoojinDeve <jwj060111@gmail.com>
2023-01-27 19:44:58 +0900

    feat: 토큰 서비스 로직 테스트

--- backend/src/test/java/com/carrot/infrastructure/jwt/service/TokenServiceTest.java
+++ backend/src/test/java/com/carrot/infrastructure/jwt/service/TokenServiceTest.java
@@ -1,7 +1,36 @@
 package com.carrot.infrastructure.jwt.service;
 
+import com.carrot.support.ServiceTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-class TokenServiceTest {
+@DisplayName("[Service] TokenService")
+class TokenServiceTest extends ServiceTest {
 
+    @InjectMocks
+    private TokenService tokenService;
+
+    @Mock
+    private TokenCreator tokenCreator;
+
+    @DisplayName("[Success] 토큰 정보 추출 요청")
+    @Test
+    void givenAccessToken_whenParsing_thenGetUserId(){
+        //given
+        String accessTokenFixture = "dummyAccessToken";
+
+        //when
+        when(tokenCreator.extractPayload(any())).thenReturn("1");
+
+        //then
+        Assertions.assertThatCode(() -> tokenService.extractUserId(accessTokenFixture))
+                .doesNotThrowAnyException();
+    }
 }
