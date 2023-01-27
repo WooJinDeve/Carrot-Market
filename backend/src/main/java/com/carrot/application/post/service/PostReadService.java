d1be9e510d7fab4f9c6af0fc687efdea5e1772b3
WoojinDeve <jwj060111@gmail.com>
2023-01-27 16:19:04 +0900

    feat: 거래 이력 조회 기능

--- backend/src/main/java/com/carrot/application/post/service/PostReadService.java
+++ backend/src/main/java/com/carrot/application/post/service/PostReadService.java
@@ -2,6 +2,7 @@ package com.carrot.application.post.service;
 
 import com.carrot.application.post.domain.Category;
 import com.carrot.application.post.domain.entity.Post;
+import com.carrot.application.post.domain.entity.TransactionHistory;
 import com.carrot.application.post.repository.PostRepository;
 import com.carrot.application.post.repository.TransactionHistoryRepository;
 import com.carrot.application.user.service.UserReadService;
@@ -15,6 +16,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.carrot.presentation.response.PostResponse.*;
+import static com.carrot.presentation.response.TransactionHistoryResponse.TransactionHistoryListResponse;
+import static com.carrot.presentation.response.TransactionHistoryResponse.TransactionHistoryListResponses;
 
 @Service
 @Transactional(readOnly = true)
@@ -40,19 +43,31 @@ public class PostReadService {
                 .build();
     }
 
-    public PostListResponses findByPurchaseHistories(final Long userId) {
-
-        return null;
-    }
-
-    public PostListResponses findBySalesHistories(final Long userId) {
-
-        return null;
-    }
-
     private List<PostListResponse> convertToPostListResponse(final List<Post> posts) {
         return posts.stream()
                 .map(PostListResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public TransactionHistoryListResponses findByPurchaseHistories(final Long userId, final Pageable pageable) {
+        Slice<TransactionHistory> transactionHistories = transactionHistoryRepository.findAllByBuyerIdOrderByIdDesc(userId, pageable);
+        return TransactionHistoryListResponses.builder()
+                .response(convertToTransactionHistoryListResponse(transactionHistories.getContent()))
+                .hasNext(transactionHistories.hasNext())
+                .build();
+    }
+
+    public TransactionHistoryListResponses findBySalesHistories(final Long userId, final Pageable pageable) {
+        Slice<TransactionHistory> transactionHistories = transactionHistoryRepository.findAllBySellerIdOrderByIdDesc(userId, pageable);
+        return TransactionHistoryListResponses.builder()
+                .response(convertToTransactionHistoryListResponse(transactionHistories.getContent()))
+                .hasNext(transactionHistories.hasNext())
+                .build();
+    }
+
+    private List<TransactionHistoryListResponse> convertToTransactionHistoryListResponse(final List<TransactionHistory> histories) {
+        return histories.stream()
+                .map(TransactionHistoryListResponse::of)
                 .collect(Collectors.toList());
     }
 }
--- backend/src/main/java/com/carrot/presentation/controller/TransactionHistoryController.java
+++ backend/src/main/java/com/carrot/presentation/controller/TransactionHistoryController.java
@@ -4,12 +4,13 @@ import com.carrot.application.post.service.PostReadService;
 import com.carrot.application.user.dto.LoginUser;
 import com.carrot.global.common.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.carrot.presentation.response.PostResponse.PostListResponses;
+import static com.carrot.presentation.response.TransactionHistoryResponse.TransactionHistoryListResponses;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,14 +20,16 @@ public class TransactionHistoryController {
     private final PostReadService postReadService;
 
     @GetMapping("/purchase-history")
-    public Response<PostListResponses> getBuyingHistories(@AuthenticationPrincipal LoginUser loginUser){
-        PostListResponses responses = postReadService.findByPurchaseHistories(loginUser.getId());
+    public Response<TransactionHistoryListResponses> getBuyingHistories(@AuthenticationPrincipal LoginUser loginUser,
+                                                                        final Pageable pageable){
+        TransactionHistoryListResponses responses = postReadService.findByPurchaseHistories(loginUser.getId(), pageable);
         return Response.success(responses);
     }
 
     @GetMapping("/sale-history")
-    public Response<PostListResponses> getSalesHistories(@AuthenticationPrincipal LoginUser loginUser){
-        PostListResponses responses = postReadService.findBySalesHistories(loginUser.getId());
+    public Response<TransactionHistoryListResponses> getSalesHistories(@AuthenticationPrincipal LoginUser loginUser,
+                                                                       final Pageable pageable){
+        TransactionHistoryListResponses responses = postReadService.findBySalesHistories(loginUser.getId(), pageable);
         return Response.success(responses);
     }
 }
